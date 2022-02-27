eb4eaa09d763356abb4a79171ace9db697067738
Volkov Ivan <steeltruedrum@gmail.com>
2022-02-27 16:48:33 +0300

    feat: Сделать popup меню. JS код

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+let popup = document.querySelector('.popup');
+let popupCloseButton = popup.querySelector('.popup__close-btn');
+let popupProfileName = popup.querySelector('.popup__profile-name');
+let popupProfileAbout = popup.querySelector('.popup__profile-about');
+let popupSaveButton = popup.querySelector('.popup__save-btn');
+let profileTitle = document.querySelector('.profile__title');
+
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+  // Получите значение полей jobInput и nameInput из свойства value
+
+  // Выберите элементы, куда должны быть вставлены значения полей
+
+  // Вставьте новые значения с помощью textContent
+}
+
+popup.addEventListener('submit', formSubmitHandler);
