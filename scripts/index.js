00cddd8c824a760f4643e9a1cfe8e7de701a4734
Volkov Ivan <steeltruedrum@gmail.com>
2022-02-27 22:25:29 +0300

    feat: откр/закр popup окна

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,19 @@
+console.log('test');
 let popup = document.querySelector('.popup');
 let popupCloseButton = popup.querySelector('.popup__close-btn');
 let popupProfileName = popup.querySelector('.popup__profile-name');
 let popupProfileAbout = popup.querySelector('.popup__profile-about');
 let popupSaveButton = popup.querySelector('.popup__save-btn');
 let profileTitle = document.querySelector('.profile__title');
+let profileEditButton = document.querySelector('.profile__edit-btn');
 
+function togglePopup() {
+  popup.classList.toggle('popup_opened');
+};
+
+profileEditButton.addEventListener('click', togglePopup);
+
+popupCloseButton.addEventListener('click', togglePopup);
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
